cefe16e8cc0b37452b2310941fbf727f694582ad
Zemuldo <danstan.otieno@gmail.com>
2019-04-18 01:02:04 +0300

    adds docs

--- lib/data_types.ex
+++ lib/data_types.ex
@@ -1,49 +1,68 @@
-defmodule Iso8583.DataTypes do
-  def test?("a", character) do
+defmodule DataTypes do
+  @moduledoc """
+  This module provides utilities for validation ISO 8583 field data types based the description below pulled
+  from a postilion interface documentation. Each character gets validated against the regex that defines each fata type.
+
+  - a - Alphabetic characters, A through Z and a through z
+  - n - Numeric digits, 0 through 9
+  - p - Pad character, space
+  - s - Special characters, i.e. other printable
+  - an - Alphabetic and numeric characters
+  - as - Alphabetic and special characters
+  - ns - Numeric and special characters
+  - anp - Alphabetic, numeric and pad characters
+  - ans - Alphabetic, numeric and special characters
+  - x C for credit, D for debit, always associated with a numeric amount field, i.e. x+n16 means a prefix of C or D followed by 16
+  numeric characters.
+  - b - Binary representation of data
+  - z - Track 2 as defined in ISO 7813
+  """
+
+  defp test?("a", character) do
     Regex.match?(~r/[a-z]/i, character)
   end
 
-  def test?("n", character) do
+  defp test?("n", character) do
     Regex.match?(~r/[0-9]/i, character)
   end
 
-  def test?("b", character) do
+  defp test?("b", character) do
     Regex.match?(~r/[0-9ABCDEF]/i, character)
   end
 
-  def test?("p", character) do
+  defp test?("p", character) do
     Regex.match?(~r/[*#]/i, character)
   end
 
-  def test?("an", character) do
+  defp test?("an", character) do
     Regex.match?(~r/[0-9a-z]/i, character)
   end
 
-  def test?("ans", character) do
+  defp test?("ans", character) do
     Regex.match?(~r/[0-9a-z-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]/i, character)
   end
 
-  def test?("ns", character) do
+  defp test?("ns", character) do
     Regex.match?(~r/[0-9-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]/i, character)
   end
 
-  def test?("s", character) do
+  defp test?("s", character) do
     Regex.match?(~r/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]/i, character)
   end
 
-  def test?("anp", character) do
+  defp test?("anp", character) do
     Regex.match?(~r/[0-9a-z*#\x20]/i, character)
   end
 
-  def test?("x+n", character) do
+  defp test?("x+n", character) do
     Regex.match?(~r/[0-9a-z*#\x20]/i, character)
   end
 
-  def test?(type, _) do
+  defp test?(type, _) do
     {:error, "Data type #{type} is not implemented"}
   end
 
-  def each(field, type, [head | tail]) do
+  defp each(field, type, [head | tail]) do
     case test?(type, head) do
       true -> each(field, type, tail)
       false -> {:error, "While processing field #{field} data provided is not of type '#{type}'"}
@@ -51,9 +70,9 @@ defmodule Iso8583.DataTypes do
     end
   end
 
-  def each(_field, _type, []), do: true
+  defp each(_field, _type, []), do: true
 
-  def run_validation(field, type, string_data) do
+  defp run_validation(field, type, string_data) do
     chars_list = String.graphemes(string_data)
 
     case String.graphemes(string_data) do
@@ -61,6 +80,19 @@ defmodule Iso8583.DataTypes do
       _ -> each(field, type, chars_list)
     end
   end
+
+  @doc """
+
+  ## Examples
+
+      iex> DataTypes.valid?("67", "x+n", "cAAAssdsd")
+      true
+      iex> DataTypes.valid?("67", "x+n", "AAAs&sdsd")
+      {:error, "Data type x+n must be presceeded with c or d"}
+      iex> DataTypes.valid?("67", "x+n", "c%%")
+      {:error, "While processing field 67 data provided is not of type 'x+n'"}
+
+  """
 
   def valid?(field, "x+n", string_data) do
     case Regex.match?(~r/[c,d]/i, String.at(string_data, 0)) do
