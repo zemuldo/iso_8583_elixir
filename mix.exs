17eb77a3b310c062737e3ed618a409971eb11d62
Danstan Onyango <danstan.otieno@gmail.com>
2020-02-22 20:06:42 +0300

    undo version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ISO8583.MixProject do
   def project do
     [
       app: :iso_8583,
-      version: "0.2.0",
+      version: "0.1.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
