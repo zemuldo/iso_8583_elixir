cc0e6b883eabd336f1ef665ec08f6add5f7ad7d1
Danstan Onyango <danstan.otieno@gmail.com>
2020-03-01 15:53:47 +0300

    adds docs and tests for mti module

--- lib/iso_8583/message/mti.ex
+++ lib/iso_8583/message/mti.ex
@@ -1,4 +1,100 @@
 defmodule ISO8583.Message.MTI do
+  @moduledoc ~S"""
+  Defines supported Message Type Identifiers (MTIs). The following ate supported MTIs.
+
+  ## Privat Types
+    
+       -  "9900": "Private use, reserved by ISO, request, Acquirer",
+       -  "9901": "Private use, reserved by ISO, request, Acquirer repeat",
+       -  "9902": "Private use, reserved by ISO, request, Issuer",
+       -  "9903": "Private use, reserved by ISO, request, Issuer repeat",
+       -  "9904": "Private use, reserved by ISO, request, Other",
+       -  "9905": "Private use, reserved by ISO, request, Other repeat",
+       -  "9910": "Private use, reserved by ISO, request response, Acquirer",
+       -  "9911": "Private use, reserved by ISO, request response, Acquirer repeat",
+       -  "9912": "Private use, reserved by ISO, request response, Issuer",
+       -  "9913": "Private use, reserved by ISO, request response, Issuer repeat",
+       -  "9914": "Private use, reserved by ISO, request response, Other",
+       -  "9915": "Private use, reserved by ISO, request response, Other repeat",
+       -  "9920": "Private use, reserved by ISO, advice, Acquirer",
+       -  "9921": "Private use, reserved by ISO, advice, Acquirer repeat",
+       -  "9922": "Private use, reserved by ISO, advice, Issuer",
+       -  "9923": "Private use, reserved by ISO, advice, Issuer repeat",
+       -  "9924": "Private use, reserved by ISO, advice, Other",
+       -  "9925": "Private use, reserved by ISO, advice, Other repeat",
+       -  "9930": "Private use, reserved by ISO, advice response, Acquirer",
+       -  "9931": "Private use, reserved by ISO, advice response, Acquirer repeat",
+       -  "9932": "Private use, reserved by ISO, advice response, Issuer",
+       -  "9933": "Private use, reserved by ISO, advice response, Issuer repeat",
+       -  "9934": "Private use, reserved by ISO, advice response, Other",
+       -  "9935": "Private use, reserved by ISO, advice response, Other repeat",
+       -  "9940": "Private use, reserved by ISO, notification, Acquirer",
+       -  "9941": "Private use, reserved by ISO, notification, Acquirer repeat",
+       -  "9942": "Private use, reserved by ISO, notification, Issuer",
+       -  "9943": "Private use, reserved by ISO, notification, Issuer repeat",
+       -  "9944": "Private use, reserved by ISO, notification, Other",
+       -  "9945": "Private use, reserved by ISO, notification, Other repeat",
+       -  "9950": "Private use, reserved by ISO, notification response, Acquirer",
+       -  "9951": "Private use, reserved by ISO, notification response, Acquirer repeat",
+       -  "9952": "Private use, reserved by ISO, notification response, Issuer",
+       -  "9953": "Private use, reserved by ISO, notification response, Issuer repeat",
+       -  "9954": "Private use, reserved by ISO, notification response, Other",
+       -  "9955": "Private use, reserved by ISO, notification response, Other repeat"
+
+  ## 1987 Version MTIs
+       - "0100": "Authorization request",
+       - "0110": "Authorization request response",
+       - "0101": "Authorization request repeat",
+       - "0120": "Authorization advice",
+       - "0121": "Authorization advice repeat",
+       - "0130": "Authorization advice response",
+       - "0200": "Financial request",
+       - "0201": "Financial request repeat",
+       - "0202": "Financial completion request",
+       - "0203": "Financial completion request Repeat",
+       - "0210": "Financial request response",
+       - "0212": "Financial completion response",
+       - "0220": "Financial Transaction Advice",
+       - "0221": "Financial Transaction Advice Repeat",
+       - "0230": "Financial Transaction advice response",
+       - "0320": "Acquirer File Update Advice",
+       - "0321": "Acquirer File Update Advice Repeat",
+       - "0322": "Issuer file update advice",
+       - "0323": "Issuer file update advice repeat",
+       - "0330": "Acquirer File Update Advice Response",
+       - "0332": "Issuer File Update Advice Response",
+       - "0400": "Reversal request",
+       - "0401": "Reversal request Repeat",
+       - "0410": "Reversal request response",
+       - "0420": "Reversal advice",
+       - "0421": "Reversal advice Repeat",
+       - "0430": "Reversal advice response",
+       - "0500": "Acquirer reconciliation request",
+       - "0501": "Acquirer reconciliation request Repeat",
+       - "0510": "Acquirer reconciliation request response",
+       - "0520": "Acquirer reconciliation advice",
+       - "0521": "Acquirer reconciliation advice repeat",
+       - "0522": "Card issuer reconciliation advice",
+       - "0532": "Card issuer reconciliation advice response",
+       - "0523": "Card issuer reconciliation advice repeat",
+       - "0530": "Acquirer reconciliation advice response",
+       - "0600": "Administration request",
+       - "0601": "Administration request repeat",
+       - "0610": "Administration response",
+       - "0620": "Administration advice",
+       - "0621": "Administration advice repeat",
+       - "0630": "Administration advice response",
+       - "0800": "Network Management request",
+       - "0801": "Network Management request repeat",
+       - "0810": "Network Management request response",
+       - "0820": "Network Management advice"
+    
+  ## 1993 Version MTIs
+    Similar to 1987 version MTIs but presceeded with 1
+
+  ## 2003 Version MTIs
+    Similar to 1987 version MTIs but presceeded with 2
+  """
   @private_common_types %{
     "9900": "Private use, reserved by ISO, request, Acquirer",
     "9901": "Private use, reserved by ISO, request, Acquirer repeat",
@@ -185,6 +281,15 @@ defmodule ISO8583.Message.MTI do
     }
   }
 
+  @doc """
+  Function check validity of MTI.
+  ## Examples
+      iex> ISO8583.Message.MTI.is_valid?(:"0100")
+      true
+      iex>ISO8583.Message.MTI.is_valid?(:"3100")
+      false
+  """
+  @spec is_valid?(mti :: String.t() | atom()) :: true | false
   def is_valid?(mti) when is_binary(mti) do
     mti = mti |> String.to_atom()
 
@@ -207,6 +312,15 @@ defmodule ISO8583.Message.MTI do
 
   def is_valid?(_), do: false
 
+  @doc """
+  Function get description of MTI.
+  ## Examples
+      iex> ISO8583.Message.MTI.get(:"0100")
+      "Authorization request"
+      iex>ISO8583.Message.MTI.get(:"3100")
+      nil
+  """
+  @spec get(mti :: String.t() | atom()) :: String.t() | nil
   def get(mti) when is_binary(mti) do
     mti
     |> String.to_atom()
